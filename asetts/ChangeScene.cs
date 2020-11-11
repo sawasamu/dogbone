72ec73a732ec0568ac25a48d96e43ee4b547331b
sawasamu <60416802+sawasamu@users.noreply.github.com>
2020-11-12 03:57:22 +0900

    Add files via upload

---
+++ asetts/ChangeScene.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeScene : MonoBehaviour
+{
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.anyKey)
+        {
+            FadeManager.Instance.LoadScene("MiniGame", 2.0f);
+
+        }
+    }
+
+}
